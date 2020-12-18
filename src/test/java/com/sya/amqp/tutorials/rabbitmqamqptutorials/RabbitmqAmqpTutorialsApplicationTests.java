6b2da2c96230e1b4b5d472723384fcbc9209ac15
ammar_sya <syedyacoob91@gmail.com>
2020-12-18 23:40:42 +0530

    Initial commit

---
+++ src/test/java/com/sya/amqp/tutorials/rabbitmqamqptutorials/RabbitmqAmqpTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sya.amqp.tutorials.rabbitmqamqptutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqAmqpTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
